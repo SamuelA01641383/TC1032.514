b3a92b60db10539494c77907040c849742452136
SamuelA01641383 <a01641383@tec.mx>
2022-10-13 13:14:41 +0000

    Practica 4

---
+++ popen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    FILE *lsOutput;
+    FILE *tomayInput;
+    char readBuffer[80];
+    lsOutput = popen("ls *.c","r");
+    tomayInput = popen("wc -l", "w");
+    while(fgets(readBuffer,80,lsOutput)){
+        fputs(readBuffer, tomayInput); //Cadena que quieres y el archivo. 
+    }
+    pclose(lsOutput);
+    pclose(tomayInput);
+}
